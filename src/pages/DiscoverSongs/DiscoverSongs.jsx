60076995abe3d227b60b3fc27ccf97036b7d8efe
mlgs1d <147573701+mlgs1d@users.noreply.github.com>
2023-11-30 08:00:02 -0500

    bug fixes

--- src/pages/DiscoverSongs/DiscoverSongs.jsx
+++ src/pages/DiscoverSongs/DiscoverSongs.jsx
@@ -6,7 +6,6 @@ import './DiscoverSongs.css';
 // Initialize Supabase client
 const supabase = createClient('https://wdnuvorjubabcofbygev.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbnV2b3JqdWJhYmNvZmJ5Z2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxODc2MDAsImV4cCI6MjAxNTc2MzYwMH0.16lHDcYbaiQZOdO7E4BYZx2R3MjJ5acqD094Wt6S5mI');
 
-<<<<<<< HEAD
 export default function Discover() {
   const { user } = useAuth();
   const [allSongs, setAllSongs] = useState([]); 
@@ -238,54 +237,6 @@ export default function Discover() {
         <button className="genre-button" onClick={() => setShowGenrePopup(true)}>
           Genre
         </button>
-=======
-
-export default function Discover() {
-  const { user } = useAuth()
-  const [videoURLs, setVideoURLs] = useState([]);
-
-  useEffect(() => {
-    // Function to fetch video URLs from Supabase
-    const fetchVideoURLs = async () => {
-      const { data, error } = await supabase
-        .from('songs')
-        .select('video_url');
-      if (error) {
-        console.error('Error fetching videos:', error);
-      } else {
-        const urls = data.map((song) => song.video_url);
-        setVideoURLs(urls);
-      }
-    };
-    fetchVideoURLs();
-  }, []);
-
-  // Proecessing short vs standard URL links
-  const videoIDs = videoURLs.map(url => {
-    // Check if URL is a short YouTube URL
-    if (url.includes('youtu.be')) {
-      return url.split('/').pop().split('?')[0];
-    }
-    // Otherwise, assume it's a standard YouTube URL and extract the 'v' parameter
-    const urlParams = new URLSearchParams(new URL(url).search);
-    return urlParams.get('v');
-  });
-
-  return (
-    <div className="discover-container">
-      <div className="discover-grid">
-        {videoIDs.map((id) => (
-          <div key={id} className="video-wrapper">
-            <iframe
-              className="video-iframe"
-              src={`https://www.youtube.com/embed/${id}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ))}
->>>>>>> d22b85091668b19f80abdc5fde68008f6825cb50
       </div>
     </div>
   );
